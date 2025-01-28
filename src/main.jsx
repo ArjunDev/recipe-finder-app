ad291feea4783a0d3f9df8d8d5ca721fee9d9f26
Arjun <simplegamerofficial97@gmail.com>
2025-01-28 15:25:47 +0530

    added replaced BrowserRouter with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter} from 'react-router-dom';
+import { HashRouter} from 'react-router-dom';
 import App from './components/App';
 import GlobalState from './components/context';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GlobalState>
         <App/>
       </GlobalState> 
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
